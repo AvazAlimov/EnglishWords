0081e59eb4cd0a8bb24d4e34e7831739f3a6abd8
LNasiba <lutfullaeva.inha@gmail.com>
2016-11-29 17:41:25 +0400

    Nasiba commited

--- src/sample/Game.java
+++ src/sample/Game.java
@@ -1,14 +1,18 @@
 package sample;
 
 import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class Game implements Initializable{
 
+    public Label word;
+
     @Override
     public void initialize(URL location, ResourceBundle resources) {
+        word.setText("Word Game");
 
     }
 }
